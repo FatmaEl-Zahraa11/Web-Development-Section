3dd715b9b1dca6f83dd5f8362bfddf74f9e10fd6
FatmaEl-Zahraa11 <totaatwan8@gmail.com>
2025-12-02 20:45:12 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function popup1()
+{
+    window.alert("You're in the Home page.");
+}
+function popup2()
+{
+    window.alert("");
+}
+
+function popup3()
+{
+    window.alert("");
+}
+
+function popup4()
+{
+    window.alert("");
+}
+
