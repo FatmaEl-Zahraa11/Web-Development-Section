a87a787f91b611d13a18f5a8b3c519e86fc8fadd
omar_tamer <otamer590@gmail.com>
2025-12-04 23:10:23 +0200

    first line of commit message

---
+++ script.js
@@ -0,0 +1,15 @@
+function html_description() {
+    window.location.href = "html-desc.html";
+}
+function css_description() {
+    window.location.href = "css-desc.html";
+}
+function js_description() {
+    window.location.href = "js-desc.html";
+}
+
+
+
+function purchase_course(){
+    window.alert("You have successfully purchase this course");
+}
